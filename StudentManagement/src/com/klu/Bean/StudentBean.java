75b25d28d5489571fbea58cab853b9c7229269bd
venkataramanavgit <74000097+venkataramanavgit@users.noreply.github.com>
2021-01-22 13:55:55 +0530

    task given on 21/01/2021
    Jdbc operations using mysql

---
+++ StudentManagement/src/com/klu/Bean/StudentBean.java
@@ -0,0 +1,27 @@
+package com.klu.Bean;
+
+public class StudentBean {
+
+	 private int regno;
+	 private String name;
+	 private String branch;
+	public int getRegno() {
+		return regno;
+	}
+	public void setRegno(int regno) {
+		this.regno = regno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	 
+}
